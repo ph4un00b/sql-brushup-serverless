d817fc92537a792ee9933434d87e375a800b61ae
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-03-20 16:13:13 -0600

    linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,8 @@ const config = {
   overrides: [
     {
       extends: [
+        "eslint:recommended",
+        "plugin:react-hooks/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
       files: ["*.ts", "*.tsx"],
@@ -18,7 +20,7 @@ const config = {
   parserOptions: {
     project: path.join(__dirname, "tsconfig.json"),
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
@@ -29,6 +31,20 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: ["off", "smart"],
+    "react/no-unescaped-entities": ["warn", { forbid: ["'"] }],
+    "react-hooks/exhaustive-deps": "error",
+    "prettier/prettier": ["off"],
+    "import/order": ["error"],
+    "no-return-await": "error",
+    "no-labels": ["off"],
+    "no-unused-labels": ["error"],
+    "@typescript-eslint/ban-ts-comment": ["off"],
+    "@typescript-eslint/no-var-requires": ["error"],
+    "@typescript-eslint/no-non-null-assertion": "off",
   },
 };
 
