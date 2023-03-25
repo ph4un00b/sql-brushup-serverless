e03192ae07977ab792ff541c91cbe472d3af051e
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-03-24 21:16:57 -0600

    ignore types and linting on vercel && minify

--- next.config.mjs
+++ next.config.mjs
@@ -6,17 +6,20 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+	/**
+	 * If you have the "experimental: { appDir: true }" setting enabled, then you
+	 * must comment the below `i18n` config out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
+	typescript: { ignoreBuildErrors: true },
+	eslint: { ignoreDuringBuilds: true },
+	swcMinify: true
 };
 export default config;
