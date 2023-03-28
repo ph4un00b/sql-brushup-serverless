164afbc72d7c83d85c56763a84e25bf32b651f0b
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-03-28 10:59:16 -0600

    encryptor

--- src/server/api/routers/join.ts
+++ src/server/api/routers/join.ts
@@ -42,8 +42,10 @@ export const innerJoinRouter = createTRPCRouter({
 		const { rows, time } = await conn.execute("SELECT * FROM User JOIN Cat;");
 		const serverQueryTime = performance.now() - queryStart;
 		await sleep(3000);
-		console.log({ time, serverQueryTime });
-		return { time, serverQueryTime, rows };
+
+		const out = { tag: "join", time, serverQueryTime };
+		console.log(out);
+		return { ...out, rows };
 	}),
 
 	/**
@@ -79,8 +81,10 @@ export const innerJoinRouter = createTRPCRouter({
 		);
 		const serverQueryTime = performance.now() - queryStart;
 		await sleep(3000);
-		console.log({ time, serverQueryTime });
-		return { time, serverQueryTime, rows };
+
+		const out = { tag: "join-on", time, serverQueryTime };
+		console.log(out);
+		return { ...out, rows };
 	}),
 	joinOnWhere: publicProcedure.query(async () => {
 		const queryStart = performance.now();
@@ -97,11 +101,115 @@ export const innerJoinRouter = createTRPCRouter({
 		);
 		const serverQueryTime = performance.now() - queryStart;
 		await sleep(3000);
-		console.log({ time, serverQueryTime });
-		return { time, serverQueryTime, rows };
+
+		const out = { tag: "join-on-where", time, serverQueryTime };
+		console.log(out);
+		return { ...out, rows };
+	}),
+	joinN1: publicProcedure.query(async () => {
+		const { rows, serverQueryTime } = await peopleAndCats();
+		await sleep(3000);
+
+		const out = { tag: "join-n+1", serverQueryTime };
+		console.log(out);
+		return { ...out, rows };
+	}),
+	joinN1_2Q: publicProcedure.query(async () => {
+		const { rows, serverQueryTime } = await peopleAndCats2Queries();
+		await sleep(3000);
+
+		const out = { tag: "join-n+1-2-queries", serverQueryTime };
+		console.log(out);
+		if (enc("012345", 1) !== "135024") throw "bad!";
+
+		return { ...out, rows };
 	}),
 
 	getSecretMessage: protectedProcedure.query(() => {
 		return "you can now see this secret message!";
 	}),
 });
+
+async function peopleAndCats2Queries() {
+	const queryStart = performance.now();
+	const conn = db.connection();
+	const results = [];
+	const { rows: people } = await conn.execute("SELECT * FROM User");
+	const { rows: cats } = await conn.execute("SELECT * FROM Cat");
+	for (const person of people) {
+		for (const cat of cats) {
+			if (cat.ownerId === person.id) {
+				results.push({ person: person.name, cat: cat.name });
+			}
+		}
+	}
+	const serverQueryTime = performance.now() - queryStart;
+	return { rows: results, serverQueryTime };
+}
+
+async function peopleAndCats() {
+	const queryStart = performance.now();
+	const conn = db.connection();
+	const results = [];
+	const { rows: people } = await conn.execute("SELECT * FROM User");
+	for (const person of people) {
+		const { rows: catsOwnedByPerson } = await conn.execute(
+			"SELECT * FROM Cat WHERE Cat.ownerId = ?",
+			[person.id],
+		);
+
+		for (const cat of catsOwnedByPerson) {
+			results.push({ person: person.name, cat: cat.name });
+		}
+	}
+	const serverQueryTime = performance.now() - queryStart;
+	return { rows: results, serverQueryTime };
+}
+
+function enc(S: string) {
+	const len = S.length;
+	const res = new Array(S.length).fill("");
+	// oeoeoe
+	const middle = Math.floor(len / 2);
+
+	// for (let index = 0; index < S.length; index++) {
+	// 	const letra = S[index];
+	// 	if (index % 2 == 0) {
+	// 		res[middle] = letra;
+	// 		middle++;
+	// 	} else {
+	// 		res[left] = letra;
+	// 		left++;
+	// 	}
+	// }
+
+	let par = middle;
+	let impar = -1;
+
+	const moves = new Array(S.length).fill(0);
+	for (let index = 0; index < middle * 2; index++) {
+		if (index % 2 === 0) {
+			console.log(par);
+			moves[index] = par;
+			par--;
+		} else {
+			console.log(impar);
+			moves[index] = impar;
+			impar--;
+		}
+	}
+
+
+	for (let index = 0; index < res.length; index++) {
+		console.log({ index, m: moves[index], diff: moves[index] + index });
+		res[moves[index]] = S[index];
+	}
+
+	// eeeooo
+	console.log({ res, moves });
+	return res.join("");
+}
+
+function dec(S: string) {
+
+}
